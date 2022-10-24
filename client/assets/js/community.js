c022fdb2cffa40aefebec0a2bc4a44b2d69b7647
DayoTitiloye-code <DayoTitiloye@outlook.com>
2022-10-24 16:19:24 +0100

    Added gifs to community form

--- client/assets/js/community.js
+++ client/assets/js/community.js
@@ -3,9 +3,50 @@ const postButton = document.querySelector('#post-button')
 const exitButton = document.querySelector('#exit-button')
 const sendButton = document.querySelector('#send-button')
 const testButton = document.querySelector('#test-send')
+const gifButton = document.querySelector('#btn-gif')
+let apiKey = `NLls5Old8idRIHlgjN8gBEsyCHM6MlSH`
+let search = document.querySelector('#search')
 postButton.addEventListener('click', showForm)
 exitButton.addEventListener('click', hideForm)
 testButton.addEventListener('click', sendData)
+gifButton.addEventListener('click', (e) =>{
+    e.preventDefault()
+    getGif()
+})
+
+function getGif(){
+
+    fetch(`https://api.giphy.com/v1/gifs/search?q=${search.value}&api_key=${apiKey}&rating=pg&limit=10`)
+    .then((response) => response.json())
+    .then((data) => {
+        // console.log(data)
+        let results = document.querySelector('#results')
+        let result = document.querySelector('#result')
+        
+        data.data.forEach((obj) =>{
+            console.log(obj.images.downsized.url)
+            let a = document.createElement('a')
+            let img = document.createElement('img')
+            img.style.width = '70px'
+            img.style.height = '70px'
+            let body = document.querySelector('body')
+            img.src = obj.images.original.url
+            img.alt =  obj.title
+
+            a.setAttribute('href', obj.images.downsized.url)
+            a.append(img)
+            results.append(a)
+            a.addEventListener('click', (e) =>{
+                e.preventDefault()
+                result.append(a)
+                results.style.visibility = 'hidden'
+            })
+
+        })
+        
+        search.value = ''
+    })
+}
 
 function display() {
     fetch('http://localhost:3000/')
