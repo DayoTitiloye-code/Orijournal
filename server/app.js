dc97576cb81b2ff48e5d0d4c6823d2b0752d9970
Bradley Gichangi <bradgichangi@gmail.com>
2022-10-24 10:58:35 +0100

    Working API, Changes to JS

--- server/app.js
+++ server/app.js
@@ -4,9 +4,18 @@ const cors = require('cors');
 const port = process.env.PORT || 3000;
 app.use(cors());
 
+let fs = require('fs');
+
+
+function getData() {
+    let fileName = 'data.json';
+    let data = JSON.parse(fs.readFileSync(fileName).toString());
+    console.log(data)
+    return data
+}
+
 app.get('/', (req, res) => {
-    const quotesArray = Quote.all;
-    res.send(quotesArray)
+    res.send(getData())
   })
 
 app.listen(port, () => console.log(`Now running on http://localhost:${port}`))
