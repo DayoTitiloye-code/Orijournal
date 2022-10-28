2ebe0d607d33a70ed1916b3a34c31f8e1e6bc0b7
Bradley Gichangi <bradgichangi@gmail.com>
2022-10-28 12:31:13 +0100

    Fixes

---
+++ frontend_js.spec.js
@@ -0,0 +1,53 @@
+/** @jest-environment jsdom */
+const { getNow, sendComment, sendPost, addReaction } = require('./community_functions');
+global.fetch = require('jest-fetch-mock');
+
+describe('community.js functions', () => {
+
+    test('getNow returns date as a string', () => {
+        expect(getNow() === getNow.toString())
+    })
+
+    describe ('Comments', () => {
+        test('it exists', () => {
+            expect(sendComment).toBeDefined()
+        })
+
+        test('it creates a POST request using fetch', () => {
+            sendComment("")
+            expect(fetch).toHaveBeenCalled()
+        })
+    })
+
+    describe ('Reactions', () => {
+        const testReact = JSON.stringify({
+            id: 1,
+            emoji: 'laugh',
+            type: true
+        })
+
+        test('it exists', () => {
+            expect(addReaction).toBeDefined()
+        })
+
+        test('it creates a PUT request using fetch', () => {
+            addReaction(testReact)
+            expect(fetch).toHaveBeenCalled()
+        })
+    })
+
+    describe('Posts', () => {
+
+
+        test('it exists', () => {
+            expect(sendPost).toBeDefined()
+        })
+
+        test('it creates a POST request using fetch', () => {
+            sendPost()
+            expect(fetch).toHaveBeenCalled()
+        })
+
+
+    })
+})
